40647c7302e5e63dc2e9d3db435adafc62cae7bb
Shawn Ragel <shawnrgl10@gmail.com>
2025-06-28 18:53:27 +0530

    initial git comit for movie app

---
+++ movie-rating-app/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "Arial" }}>
+      <h1>🎬 Movie Rating App</h1>
+      <p>Let's build something amazing!</p>
+    </div>
+  );
+}
+
+export default App;
